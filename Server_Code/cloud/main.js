4e06fdf31e1ef58e4da8f8c5f05a26658cc81d5a
swms <SpencerWilliams2016@u.northwestern.edu>
2015-04-14 12:13:25 -0500

    Added get_games

--- Server_Code/cloud/main.js
+++ Server_Code/cloud/main.js
@@ -20,3 +20,18 @@ Parse.Cloud.define("get_players", function(request, response)
 	});
 });
 
+Parse.Cloud.define("get_games", function(request, response)
+{
+	var query = new Parse.Query("Current_Games");
+	query.select("GameID", "Location_Name", "Sport", "Num_Of_Players", "Max_Num_Of_Players", "Start_Time", "End_Time");
+	query.ascending("Start_Time");
+	query.limit(2);
+	query.find({
+		success: function(results) {
+			response.success(results);
+		},
+		error: function() {
+			response.error("Player lookup failed");
+		}
+	});
+});
