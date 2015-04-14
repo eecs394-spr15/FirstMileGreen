214f4481af42a25913f896a8becf701a313bfc51
swms <SpencerWilliams2016@u.northwestern.edu>
2015-04-14 12:06:06 -0500

    Added get_players

--- Server_Code/cloud/main.js
+++ Server_Code/cloud/main.js
@@ -2,5 +2,21 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	response.success("Hello world!");
 });
+
+Parse.Cloud.define("get_players", function(request, response)
+{
+	var query = new Parse.Query("Players");
+	query.equalTo("GameID", request.params.GameID);
+	query.select("PlayerID");
+	query.find({
+		success: function(results) {
+			response.success(results);
+		},
+		error: function() {
+			response.error("Player lookup failed");
+		}
+	});
+});
+
